39129a39ec25056b8fba3b348b7ffe6397b21291
da07ng <gaodaojing@gmail.com>
2018-07-12 09:18:33 +0800

    feat(apollo-client): set apollo-client manually

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { onError } from 'apollo-link-error';
+import { withClientState } from 'apollo-link-state';
+import merge from 'lodash.merge';
 
 import localState from './localState';
 
 import './assets/css/semantic.min.css';
 
 import App from './App';
-
 import registerServiceWorker from './utils/registerServiceWorker';
 
-const client = new ApolloClient({
+const cache = new InMemoryCache();
+
+const link = new HttpLink({
   uri: 'http://localhost:4000/graphql',
-  clientState: localState
+  credentials: 'same-origin'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) {
+        console.log(`[Network error]: ${networkError}`);
+      }
+    }),
+    withClientState(merge(localState, { cache })),
+    authLink.concat(link)
+  ]),
+  cache
 });
 
 ReactDOM.render(
